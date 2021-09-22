4214e35b40864a2b41c0a8f4cecf915b9dfeb837
Franklin Yin <franklinyin1@gmail.com>
2021-09-21 20:14:10 -0400

    added reducer in cart store

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,19 +1,19 @@
-import axios from "axios"
+import axios from "axios";
 
 //ACTION TYPES
 // const SET_CART_COUNT = "SET_CART_COUNT"
-const CREATE_CART = "CREATE_CART"
+const CREATE_CART = "CREATE_CART";
 
 //ACTION CREATORS
-// export const CREATECartCount = (cartCount) => ({
+// export const setCartCount = (cartCount) => ({
 //   type: SET_CART_COUNT,
 //   cartCount
 // })
 
 export const _createCart = (cart) => ({
   type: CREATE_CART,
-  cart
-})
+  cart,
+});
 
 //THUNK CREATORS
 // export const fetchCartCount = () => async (dispatch) => {
@@ -26,11 +26,21 @@ export const _createCart = (cart) => ({
 
 export const createCart = (order) => async (dispatch) => {
   try {
-    const { data } = await axios.post(`/api/orders/${order.userId}`, order)
-    dispatch(_createCart(data))
+    const { data } = await axios.post(`/api/orders/${order.userId}`, order);
+    dispatch(_createCart(data));
   } catch (error) {
-    console.log(error)
+    console.log(error);
+  }
+};
+
+//REDUCER
+
+const initialState = null;
+export default function (cart = initialState, action) {
+  switch (action.type) {
+    case CREATE_CART:
+      return {};
+    default:
+      return cart;
   }
 }
-
-
