733602284015af1519e3d48db80c08b5a16e5cc6
Franklin Yin <franklinyin1@gmail.com>
2021-09-22 16:37:47 -0400

    feature: Add to Cart in SingleCandy view

--- client/components/SingleCandy.js
+++ client/components/SingleCandy.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchSingleCandy } from '../store/singleCandy';
+import { createCart } from '../store/cart'
+import { createCandyOrder } from '../store/candyOrders';
+import { updateCandyQuantity } from '../store/candyOrders'
 
 export class SingleCandy extends Component {
+
+  constructor(props) {
+    super(props)
+    this.handleAddToCart = this.handleAddToCart.bind(this)
+  }
+
   componentDidMount() {
     this.props.loadSingleCandy(this.props.match.params.candyId);
   }
+
+  async handleAddToCart(candy, userId) {
+    if (this.props.cart.id){
+      //check if the candy exists in candyOrders
+      let preexistingCandyOrder = this.props.candyOrders.reduce((accum, candyOrder) => {
+        if (candyOrder.candyId === candy.id) {
+          accum = {...candyOrder}
+          return accum
+        }
+        return accum
+      }, {})
+      if (Object.keys(preexistingCandyOrder).length > 0) {
+        //increment quantity of existing candyOrder
+        await this.props.updateCandyQuantity({...preexistingCandyOrder, quantity: preexistingCandyOrder.quantity+1})
+      } else {
+        //create new candyOrder
+        await this.props.createCandyOrder({
+          orderId: this.props.cart.id,
+          candyId: candy.id,
+          quantity: 1
+        })
+      }
+
+    } else {
+      await this.props.createCart({userId})
+      await this.props.createCandyOrder({
+        orderId: this.props.cart.id,
+        candyId: candy.id,
+        quantity: 1
+      })
+    }
+  }
+
   render() {
     const singleCandy = this.props.singleCandy;
+    const userId = this .props.userId
     return (
       <div>
         <React.Fragment>
@@ -29,7 +72,7 @@ export class SingleCandy extends Component {
                   specimen book. It has survived not on
                 </p>
                 <h3>{`Price: $ ${singleCandy.price}`}</h3>
-                <button className="add-cart-button">Add To Cart</button>
+                <button className="add-cart-button" onClick={() => this.handleAddToCart(singleCandy, userId)}>Add To Cart</button>
               </div>
             </div>
           </div>
@@ -42,12 +85,18 @@ export class SingleCandy extends Component {
 const mapState = (state) => {
   return {
     singleCandy: state.singleCandy,
+    userId: state.auth.id,
+    cart: state.cart,
+    candyOrders: state.candyOrders
   };
 };
 
 const mapDispatch = (dispatch) => {
   return {
     loadSingleCandy: (candyId) => dispatch(fetchSingleCandy(candyId)),
+    createCart: (userId) => dispatch(createCart(userId)),
+    createCandyOrder: (candyOrder) => dispatch(createCandyOrder(candyOrder)),
+    updateCandyQuantity: (candyOrder) => dispatch(updateCandyQuantity(candyOrder))
   };
 };
 
