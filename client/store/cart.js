9950f531f390cbcf725f222c3465cfef717176e5
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-09-23 22:43:36 -0400

    Protecting Routes part 2

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+const TOKEN = 'token';
+const token = window.localStorage.getItem(TOKEN);
 
 //ACTION TYPES
 // const SET_CART_COUNT = "SET_CART_COUNT"
@@ -47,17 +49,30 @@ export const setCart = (id) => async (dispatch) => {
 
 export const createCart = (order) => async (dispatch) => {
   try {
-    const { data } = await axios.post('/api/orders/', order);
-    dispatch(_createCart(data));
+    if (token) {
+      const { data } = await axios.post('/api/orders/', order, {
+        headers: {
+          authorization: token,
+        },
+      });
+      dispatch(_createCart(data));
+    }
   } catch (error) {
     console.log(error);
   }
 };
 
 export const checkoutCart = (order) => async (dispatch) => {
+  console.log(order);
   try {
-    const { data } = await axios.put('/api/orders/', order);
-    dispatch(_checkoutCart(data));
+    if (token) {
+      const { data } = await axios.put('/api/orders/', order, {
+        headers: {
+          authorization: token,
+        },
+      });
+      dispatch(_checkoutCart(data));
+    }
   } catch (error) {
     console.log(error);
   }
